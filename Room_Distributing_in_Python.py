fe62b9b6fe968c06cd0f3cf089bdff8ab01d8ea5
LearnerForever666 <82526019+LearnerForever666@users.noreply.github.com>
2021-09-27 20:45:13 +0800

    Update Room_Distributing_in_Python.py

--- Room_Distributing_in_Python.py
+++ Room_Distributing_in_Python.py
@@ -1,3 +1,9 @@
+# It is a code which may helps you to distribute your room when you are in puzzle.
+# And as you see,it is my FIRST code in Python which is uploaded to the Github.
+'''
+author: Learner
+Date: 2021 Sep 27th
+'''
 import random
 distr=[[],[],[]]
 name=['A','B','C','D','E','F','G','H']
