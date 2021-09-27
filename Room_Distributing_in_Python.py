4a03f9b5d11f3b40943770fcf2f101ed73c7f7c3
LearnerForever666 <82526019+LearnerForever666@users.noreply.github.com>
2021-09-27 20:45:29 +0800

    Update Room_Distributing_in_Python.py

--- Room_Distributing_in_Python.py
+++ Room_Distributing_in_Python.py
@@ -1,7 +1,7 @@
 # It is a code which may helps you to distribute your room when you are in puzzle.
 # And as you see,it is my FIRST code in Python which is uploaded to the Github.
 '''
-author: Learner
+author: LearnerForever
 Date: 2021 Sep 27th
 '''
 import random
